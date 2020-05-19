8590b62d93be088d49db8ecaefaef1d7bea10bdd
Joao Otavio <joaootavio22017@gmail.com>
2020-05-19 07:55:55 -0300

    Leitura do volume

--- main.cpp
+++ main.cpp
@@ -3,11 +3,15 @@
 using namespace std;
 
 int main()
-{
+{   int a, b, c;
+    double d;
+    string e;
     cout << "SISTEMA DE PERFIL DE CLIENTES" << endl;
     cout << endl<<"-----------------------------" << endl;
     cout << endl<<"INFORME OS DADOS DO ULTIMO ANO" << endl;
-
-
+    cout << endl<<"Quantas compras o cliente fez no ultimo ano?" << endl;
+    cin>>a;
+    cout << endl<<"Qual o ticket medio?" << endl;
+    cin>>d;
     return 0;
 }
